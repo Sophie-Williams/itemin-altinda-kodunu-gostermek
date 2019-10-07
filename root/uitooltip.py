12eee845f97366807fd6d9ae9a58aa5f759e85e0
Mertcan Güler <barntrlol@gmail.com>
2019-10-07 20:24:15 +0300

    itemin-altinda-kodunu-gostermek

---
+++ root/uitooltip.py
@@ -0,0 +1,13 @@
+# Arat -- Birden fazla olabilir. " def __DragonSoulInfoString (self, dwVnum): " üstünde olanı bulun:
+		self.ShowToolTip()
+
+# Üzerine ekle:
+		if chr.IsGameMaster(player.GetMainCharacterIndex()):
+			self.AppendTextLine(localeInfo.ITEM_VNUM_TOOLTIP % (int(itemVnum)), self.SINIRSIZ_COLOR)
+# Örnek: https://prnt.sc/pg2y2a
+
+# Arat:
+	NEED_SKILL_POINT_COLOR = 0xff9A9CDB
+
+# Altına ekle:
+	SINIRSIZ_COLOR = 0xffFFFF00
